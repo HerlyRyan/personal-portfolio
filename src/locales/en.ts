e2a3a41b7ae3c7a3ed86d4f381419212a4894a65
HerlyRyan <herlyryanth@gmail.com>
2026-08-25 14:30:00 +0800

    Modified: implement data locale for translation

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,12 +1,22 @@
+import { aboutEn } from './data/aboutData';
+import { experienceId } from './data/experienceData';
+import { projectsEn } from './data/projectsData';
+import { skills } from './data/skillsData';
+
 export const en = {
   profileSidebar: {
-    status: 'Available'
+    status: 'Available',
   },
   systemOverview: {
-    tag: "// SYSTEM OVERVIEW",
-    badge: "Backend & Architecture",
-    title: "Engineering scalable systems with precision.",
-    description: "Focused on designing robust backend architecture, structured database management, and high-performance system integration to fully support enterprise productivity.",
-    secureConnection: "[Secure Connection]"
-  }
+    tag: '// SYSTEM OVERVIEW',
+    badge: 'Backend & Architecture',
+    title: 'Building scalable systems with precision.',
+    description:
+      'Focused on designing robust backend architecture, structured database management, and high-performance system integration to fully support enterprise productivity.',
+    secureConnection: '[Secure Connection]',
+  },
+  projectsModal: projectsEn,
+  experienceModal: experienceId,
+  skillsModal: skills,
+  aboutModal: aboutEn,
 };
